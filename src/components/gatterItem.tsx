158c5e38bb1225712af4627108aea8e4d281bfd7
Andreas Friedel <friedel.pharmedsolutions@gmail.com>
2019-09-05 12:27:58 +0200

    Some gatter shapes

--- src/components/gatterItem.tsx
+++ src/components/gatterItem.tsx
@@ -33,6 +33,10 @@ const themedClasses = (theme: Theme) => ({
 		fill: theme.svg.fillWhite
 	},
 
+	noFill: {
+		fill: theme.svg.none
+	},
+
 	itemEditorContainer: {
 		flex: "0 0 10rem",
 		display: "flex",
@@ -110,6 +114,92 @@ export const GatterItem: React.FC<GatterItemProps> = props => {
 								cx={ 5 } cy={ 40 } r={ 2 } />
 							<path className={ classNames(classes.thinLines, classes.whiteFill) }
 								d="M 5,12 L 20,12 A 15,18 0 0 1 20,48 L 5,48 Z"
+							/>
+							<circle className={ classNames(classes.thinLines, classes.whiteFill) }
+								cx={ 35 } cy={ 30 } r={ 2 } />
+						</svg>
+						<div className={ classes.itemEditorContainer }>
+							<InputPinCountEditor pinCount={ inputPinCount } onChange={ setInputPinCount } />
+						</div>
+						<div className={ classes.itemName }>{ gatterType }</div>
+					</>
+				);
+			case "Or":
+				return (
+					<>
+						<svg className={ classes.icon } viewBox="0 0 40 60">
+							<circle className={ classNames(classes.thinLines, classes.blackFill) }
+								cx={ 5 } cy={ 20 } r={ 2 } />
+							<circle className={ classNames(classes.thinLines, classes.blackFill) }
+								cx={ 5 } cy={ 40 } r={ 2 } />
+							<circle className={ classNames(classes.thinLines, classes.blackFill) }
+								cx={ 35 } cy={ 30 } r={ 2 } />
+							<path className={ classNames(classes.thinLines, classes.whiteFill) }
+								d="M 5,16 A 30,27 0 0 1 35,30 A 30,27 0 0 1 5,44 A 30,36 0 0 0 5,16 Z"
+							/>
+						</svg>
+						<div className={ classes.itemEditorContainer }>
+							<InputPinCountEditor pinCount={ inputPinCount } onChange={ setInputPinCount } />
+						</div>
+						<div className={ classes.itemName }>{ gatterType }</div>
+					</>
+				);
+			case "Nor":
+				return (
+					<>
+						<svg className={ classes.icon } viewBox="0 0 40 60">
+							<circle className={ classNames(classes.thinLines, classes.blackFill) }
+								cx={ 5 } cy={ 20 } r={ 2 } />
+							<circle className={ classNames(classes.thinLines, classes.blackFill) }
+								cx={ 5 } cy={ 40 } r={ 2 } />
+							<path className={ classNames(classes.thinLines, classes.whiteFill) }
+								d="M 5,16 A 30,27 0 0 1 35,30 A 30,27 0 0 1 5,44 A 30,36 0 0 0 5,16 Z"
+							/>
+							<circle className={ classNames(classes.thinLines, classes.whiteFill) }
+								cx={ 35 } cy={ 30 } r={ 2 } />
+						</svg>
+						<div className={ classes.itemEditorContainer }>
+							<InputPinCountEditor pinCount={ inputPinCount } onChange={ setInputPinCount } />
+						</div>
+						<div className={ classes.itemName }>{ gatterType }</div>
+					</>
+				);
+			case "Xor":
+				return (
+					<>
+						<svg className={ classes.icon } viewBox="0 0 40 60">
+							<circle className={ classNames(classes.thinLines, classes.blackFill) }
+								cx={ 5 } cy={ 20 } r={ 2 } />
+							<circle className={ classNames(classes.thinLines, classes.blackFill) }
+								cx={ 5 } cy={ 40 } r={ 2 } />
+							<circle className={ classNames(classes.thinLines, classes.blackFill) }
+								cx={ 35 } cy={ 30 } r={ 2 } />
+							<path className={ classNames(classes.thinLines, classes.whiteFill) }
+								d="M 8,16 A 29,27 0 0 1 35,30 A 29,27 0 0 1 8,44 A 30,36 0 0 0 8,16 Z"
+							/>
+							<path className={ classNames(classes.thinLines, classes.noFill) }
+								d="M 5,15 A 30,36 0 0 1 5,45"
+							/>
+						</svg>
+						<div className={ classes.itemEditorContainer }>
+							<InputPinCountEditor pinCount={ inputPinCount } onChange={ setInputPinCount } />
+						</div>
+						<div className={ classes.itemName }>{ gatterType }</div>
+					</>
+				);
+			case "Xnor":
+				return (
+					<>
+						<svg className={ classes.icon } viewBox="0 0 40 60">
+							<circle className={ classNames(classes.thinLines, classes.blackFill) }
+								cx={ 5 } cy={ 20 } r={ 2 } />
+							<circle className={ classNames(classes.thinLines, classes.blackFill) }
+								cx={ 5 } cy={ 40 } r={ 2 } />
+							<path className={ classNames(classes.thinLines, classes.whiteFill) }
+								d="M 8,16 A 29,27 0 0 1 35,30 A 29,27 0 0 1 8,44 A 30,36 0 0 0 8,16 Z"
+							/>
+							<path className={ classNames(classes.thinLines, classes.noFill) }
+								d="M 5,15 A 30,36 0 0 1 5,45"
 							/>
 							<circle className={ classNames(classes.thinLines, classes.whiteFill) }
 								cx={ 35 } cy={ 30 } r={ 2 } />
